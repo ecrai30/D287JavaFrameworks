13334bac4c5c88b0c93ac6d284c3f0440a9d8a89
Ezekiel Craig <ecrai30@wgu.edu>
2024-04-11 18:28:02 -0500

    Added BuyProductController

--- src/main/java/com/example/demo/controllers/BuyProductController.java
+++ src/main/java/com/example/demo/controllers/BuyProductController.java
@@ -1,27 +1,9 @@
-package com.example.demo.controller;
+package com.example.demo.controllers;
 
-import com.example.demo.service.ProductService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class BuyProductController {
 
-    private final ProductService productService;
 
-    @Autowired
-    public BuyProductController(ProductService productService) {
-        this.productService = productService;
-    }
-
-    @GetMapping("/decrementInventory/{productId}")
-    public String decrementProductInventory(@PathVariable int productId) {
-        productService.decrementProductInventory(productId);
-        return "redirect:/products"; // Redirect to the product list page or any other appropriate page
-    }
-
-    @PostMapping()
 }
